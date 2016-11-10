2768c8e375791768da2e9b14470b56549a5339f5
rdalin82 <Robert.Dalin@gmail.com>
2016-11-09 18:44:07 -0600

    Projects can now be deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,9 +33,15 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been updated." 
     else 
       flash[:alert] = "Project has not been updated" 
-    end
-        
+    end    
     redirect_to @project
+  end
+
+  def destroy 
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted." 
+    redirect_to projects_path
   end
 
   private 
--- spec/features/deleting_projects_spec.rb
+++ spec/features/deleting_projects_spec.rb
@@ -1,4 +1,13 @@
-# require 'rails_helper' 
+require 'rails_helper' 
 
-# RSpec.feature "Users can delete project" do 
-#   scenario "Successfully" do 
+RSpec.feature "Users can delete project" do 
+  scenario "Successfully" do 
+    FactoryGirl.create(:project, name: "Sublime Text 3") 
+
+    visit "/" 
+    click_link "Sublime Text 3" 
+    click_link "Delete Project" 
+    expect(page).to have_content "Project has been deleted." 
+    expect(page.current_url).to eq projects_url
+  end
+end
