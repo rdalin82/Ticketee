17991fda44978023464935445b621fc705e7d64a
rdalin82 <Robert.Dalin@gmail.com>
2016-11-23 19:03:19 -0600

    Added Devise + sign up feature

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+end
